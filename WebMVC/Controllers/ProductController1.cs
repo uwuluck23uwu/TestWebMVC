727e3e4ecb26b2f8474d90f23a8e4b9916b1ba2e
uwuluck23uwu <luck23000000@gmail.com>
2023-11-25 13:36:16 +0700

    Send many Object Passed to View

--- WebMVC/Controllers/ProductController1.cs
+++ WebMVC/Controllers/ProductController1.cs
@@ -9,11 +9,11 @@ namespace WebMVC.Controllers
         {
             var product = new List<Product>()
             {
-                new Product() {Id = 1, Name = "<3Gojo<3", Price = 10000, Amount = 20},
-                new Product() {Id = 2, Name = "<3Geto<3", Price = 20000, Amount = 20},
-                new Product() {Id = 3, Name = "<3Nanami<3", Price = 30000, Amount = 20}
+                new Product() {Id = 1, Name = "<3 Gojo <3", Price = 10000, Amount = 20},
+                new Product() {Id = 2, Name = "<3 Geto <3", Price = 20000, Amount = 20},
+                new Product() {Id = 3, Name = "<3 Nanami <3", Price = 30000, Amount = 20}
             };
-            return View(product);
+            return View(new {product, Title = "< สินค้า >"});
         }
     }
 }
