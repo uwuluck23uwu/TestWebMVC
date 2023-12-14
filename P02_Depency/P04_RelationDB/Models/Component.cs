74625ca435522b958be3988cc8ed531d2fcb6edb
Sumo <144433156+uwuluck23uwu@users.noreply.github.com>
2023-12-14 15:41:53 +0700

    One To One

---
+++ P02_Depency/P04_RelationDB/Models/Component.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P04_RelationDB.Models
+{
+    public class Component
+    {
+        //[Key]
+        //public int XXId { get; set; }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FeatureId { get; set; }
+        public Feature Feature { get; set; }
+    }
+}
